b0d96b5bf667d6602b5768814aec5facf66122b8
CaioVarela <caiovareladelima@gmail.com>
2022-02-26 14:44:49 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,6 @@ void main() async {
 
   runApp(MaterialApp(
     home: Home(),
-    theme: ThemeData(
-        hintColor: Colors.amber,
-        primaryColor: Colors.white,
-        inputDecorationTheme: InputDecorationTheme(
-          enabledBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-          focusedBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
-          hintStyle: TextStyle(color: Colors.amber),
-        )),
   ));
 }
 
